94feb7788e9150e3c85947409e89d5a05c757628
amankrbhagat <115861752+amankrbhagat@users.noreply.github.com>
2025-04-28 17:59:18 +0530

    Add files via upload

---
+++ Node by self/hello.js
@@ -0,0 +1,27 @@
+// to export another function we can do it as follows
+//  ./  krke import krnge to current directory me search krega jo v import krna hai.
+//  aur agar direct krnge to wo pre build- in packages me dhundega.
+
+
+// const math = require("./math")  ///  or directly we can use    ///  variable me store kiye export
+// const {addFn, subFn} = require("./math")   // object me store kiye export ko
+const {add,sub}= require("./math")  
+
+
+
+// console.log("Hello");
+// console.log("Aman");
+
+
+// console.log(math.addFn(3, 5));
+// console.log(math.subFn(3, 5));
+
+// console.log(addFn(3, 5));
+// console.log(subFn(3, 5));
+
+
+// console.log(add1);
+// console.log(sub1);
+
+// console.log(add(2,4));
+// console.log(sub(3, 1));
